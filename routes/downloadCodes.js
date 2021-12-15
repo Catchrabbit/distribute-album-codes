155829617f5d0eb0cbb858b57968c3d52eb072b4
Quinton Harris <quinton.harris@carbonfive.com>
2021-12-14 23:02:53 -0800

    Basic app structure

--- routes/downloadCodes.js
+++ routes/downloadCodes.js
@@ -1,7 +1,6 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET download codes listing. */
 router.get('/:downloadCode', function(req, res, next) {
   res.send(`download cooooode ${req.params['downloadCode']}`);
 });
