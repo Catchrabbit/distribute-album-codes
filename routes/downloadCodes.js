0989ee17868dc1b14e52eb1d43e4236e9cae9a3a
Quinton Harris <quinton.harris@carbonfive.com>
2021-12-14 23:12:52 -0800

    Form wired up

--- routes/downloadCodes.js
+++ routes/downloadCodes.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/:downloadCode', function(req, res, next) {
-  res.send(`download cooooode ${req.params['downloadCode']}`);
+  res.render('download-codes.ejs', { title: 'CATCHRABBIT' });
 });
 
 module.exports = router;
--- routes/index.js
+++ routes/index.js
@@ -1,19 +1,21 @@
 var express = require('express');
 var router = express.Router();
+var utils = require('../utils');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'CATCHRABBIT' });
 });
 
 router.post('/', function(req, res, next) {
   const downloadCode = req.body.download_code
   
-  if (downloadCode === 'aaa') {
-    res.redirect('/d/aaa')
+  if (!downloadCode) {
+    res.redirect('/')
   }
 
-  res.redirect('/')
+  const slugifiedDownloadCode = utils.slugify(downloadCode);
+  res.redirect(`/d/${slugifiedDownloadCode}`)
 });
 
 module.exports = router;
